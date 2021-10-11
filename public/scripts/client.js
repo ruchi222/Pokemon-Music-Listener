cb95c3e7fa6050fab8e8f5bab8aa80a641447a64
aphy18 <aphason1@gmail.com>
2021-10-10 20:53:56 -0400

    setting background images on click

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-  $(".gif-1").click(function(){
-    console.log("gif-1")
-    let imageURL = 'images/pikachuwallpaper.jpeg'
+  $("#red").click(function(){
+    console.log("i am body")
+    $("body").css("background-image", "none");
+    // $("body").css('background-color', '#ffffff');
+  })
+
+
+  $("#red").click(function(){
+    let imageURL = 'images/wp2403498.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $("body").css("background-size", "cover");
   })
+
+  $("#blue").click(function(){
+    let imageURL = 'images/arcaninewallpaper.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+
+
+
+
+
+
+
 })
+
