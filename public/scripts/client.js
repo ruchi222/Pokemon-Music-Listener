2578e1a910073cabe806c75fff6d95992fbb6b4d
aphy18 <aphason1@gmail.com>
2021-10-11 00:34:26 -0400

    formatting document

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
   $("#red").click(function(){
     let imageURL = 'images/wp2403498.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Red</b>");
     $(".music").prop("currentTime",0);
     $(".music")[0].play();
@@ -15,14 +14,12 @@ $(document).ready(function(){
   $("#blue").click(function(){
     let imageURL = 'images/arcaninewallpaper.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Blue</b>")
   })
 
   $("#lance").click(function(){
     let imageURL = 'images/DRAGONITE.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Lance</b>")
     // $("#music")[0].play();
   })
@@ -30,7 +27,6 @@ $(document).ready(function(){
   $("#steven").click(function(){
     let imageURL = 'images/MEGAMETA.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Steven</b>")
     $(".music")[1].play()
     $(".music")[0].pause();
@@ -39,35 +35,28 @@ $(document).ready(function(){
   $("#wallace").click(function(){
     let imageURL = 'images/MILOTIC.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Wallace</b>")
   })
 
   $("#cynthia").click(function(){
     let imageURL = 'images/GARCHOMP.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Cynthia</b>")
   })
 
   $("#alder").click(function(){
     let imageURL = 'images/VOLCARONA.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
     $(".which-trainer").html("<b>Trainer Alder</b>")
   })
 
   $("#iris").click(function(){
     let imageURL = 'images/HAXORUS.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
-    $("body").css("background-size", "cover");
+    // $("body").css("background-size", "cover");
+    // $("body").css("background-repeat", "no-repeat");
     $(".which-trainer").html("<b>Trainer Iris</b>")
   })
-
-
-
-
-
 
 
 })
