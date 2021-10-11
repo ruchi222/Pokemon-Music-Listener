0722ffe89bd5b86c61ded24607268e878d63919f
aphy18 <aphason1@gmail.com>
2021-10-10 21:15:16 -0400

    changed to vertical format

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,6 +18,41 @@ $(document).ready(function(){
     $("body").css("background-size", "cover");
   })
 
+  $("#lance").click(function(){
+    let imageURL = 'images/DRAGONITE.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+  $("#steven").click(function(){
+    let imageURL = 'images/META.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+  $("#wallace").click(function(){
+    let imageURL = 'images/MILOTIC.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+  $("#cynthia").click(function(){
+    let imageURL = 'images/GARCHOMP.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+  $("#alder").click(function(){
+    let imageURL = 'images/VOLCARONA.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
+
+  $("#iris").click(function(){
+    let imageURL = 'images/HAXORUS.jpeg'
+    $("body").css("background-image", "url(" + imageURL + ")");
+    $("body").css("background-size", "cover");
+  })
 
 
 
