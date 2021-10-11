bbc4c7a42554c77cf7987882dfa6181ad5f66745
aphy18 <aphason1@gmail.com>
2021-10-11 01:15:28 -0400

    finishing off music feature

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
- 
-  
   $("#red").click(function(){
     let imageURL = 'images/wp2403498.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
@@ -8,6 +6,12 @@ $(document).ready(function(){
     $(".music").prop("currentTime",0);
     $(".music")[0].play();
     $(".music")[1].pause();
+    $(".music")[2].pause();
+    $(".music")[3].pause();
+    $(".music")[4].pause();
+    $(".music")[5].pause();
+    $(".music")[6].pause();
+    $(".music")[7].pause();
     console.log($(".music"))
   })
 
@@ -15,39 +19,91 @@ $(document).ready(function(){
     let imageURL = 'images/arcaninewallpaper.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Blue</b>")
+    $(".music").prop("currentTime",0);
+    $(".music")[3].play();
+    $(".music")[0].pause();
+    $(".music")[1].pause();
+    $(".music")[2].pause();
+    $(".music")[4].pause();    
+    $(".music")[5].pause();    
+    $(".music")[6].pause();
+    $(".music")[7].pause();
   })
 
   $("#lance").click(function(){
     let imageURL = 'images/DRAGONITE.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Lance</b>")
-    // $("#music")[0].play();
+    $(".music").prop("currentTime",0);
+    $(".music")[1].play();
+    $(".music")[0].pause();
+    $(".music")[2].pause();
+    $(".music")[3].pause();
+    $(".music")[4].pause();    
+    $(".music")[5].pause();   
+    $(".music")[6].pause(); 
+    $(".music")[7].pause();
   })
 
   $("#steven").click(function(){
     let imageURL = 'images/MEGAMETA.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Steven</b>")
-    $(".music")[1].play()
+    $(".music").prop("currentTime",0);
+    $(".music")[2].play()
     $(".music")[0].pause();
+    $(".music")[1].pause();    
+    $(".music")[3].pause();    
+    $(".music")[4].pause();    
+    $(".music")[5].pause();    
+    $(".music")[6].pause();
+    $(".music")[7].pause();
+
   })
 
   $("#wallace").click(function(){
     let imageURL = 'images/MILOTIC.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Wallace</b>")
+    $(".music").prop("currentTime",0);
+    $(".music")[5].play()
+    $(".music")[0].pause();
+    $(".music")[1].pause()
+    $(".music")[2].pause();    
+    $(".music")[3].pause();    
+    $(".music")[4].pause(); 
+    $(".music")[6].pause();  
+    $(".music")[7].pause(); 
   })
 
   $("#cynthia").click(function(){
     let imageURL = 'images/GARCHOMP.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Cynthia</b>")
+    $(".music").prop("currentTime",0);
+    $(".music")[4].play()
+    $(".music")[0].pause();
+    $(".music")[1].pause();    
+    $(".music")[2].pause();    
+    $(".music")[3].pause();  
+    $(".music")[5].pause();  
+    $(".music")[6].pause();
+    $(".music")[7].pause();
   })
 
   $("#alder").click(function(){
     let imageURL = 'images/VOLCARONA.jpeg'
     $("body").css("background-image", "url(" + imageURL + ")");
     $(".which-trainer").html("<b>Trainer Alder</b>")
+    $(".music").prop("currentTime",0);
+    $(".music")[6].play()
+    $(".music")[0].pause();
+    $(".music")[1].pause();    
+    $(".music")[2].pause();    
+    $(".music")[3].pause();  
+    $(".music")[4].pause();  
+    $(".music")[5].pause();
+    $(".music")[7].pause();
   })
 
   $("#iris").click(function(){
@@ -56,8 +112,15 @@ $(document).ready(function(){
     // $("body").css("background-size", "cover");
     // $("body").css("background-repeat", "no-repeat");
     $(".which-trainer").html("<b>Trainer Iris</b>")
+    $(".music").prop("currentTime",0);
+    $(".music")[7].play()
+    $(".music")[0].pause();
+    $(".music")[1].pause();    
+    $(".music")[2].pause();    
+    $(".music")[3].pause();  
+    $(".music")[4].pause();  
+    $(".music")[5].pause();
+    $(".music")[6].pause();
   })
-
-
 })
 
